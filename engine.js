ecfb1eaa3f086f7293b8709cee4e896820cc6114
Natalie Russell <nlrussell97@gmail.com>
2020-04-06 19:06:48 -0500

    Speaks lyric and button opts when clicked

--- engine.js
+++ engine.js
@@ -1,11 +1,23 @@
 $(document).ready(() => {
+    let synth = window.speechSynthesis;
+
+    // read aloud the blank lyric
+    $("#speak").on("click", function() { 
+        // have to edit lyric so it reads out 'blank' rather than 'underscore' x5
+        let currentLyric = $(".lyric").text().replace(/_____/, 'blank'); 
+        let utterThis = new SpeechSynthesisUtterance(currentLyric);
+        synth.speak(utterThis);
+    });
+    
     let songs = [{}]; // this is where each lyric will be stored as an object
     let state = {songID: 0, lyricCount: 0};
     // all objects with same songID belong to the same song
 
     $(".left").on("click", function() { 
         let word = $(".left").attr("id");
+        synth.speak(new SpeechSynthesisUtterance(word));
         console.log(word);
+
         let lyric = "I have a pet " + word;
         $(".lyric").text(lyric);
         state = pushLyric(lyric, songs, state);
@@ -14,7 +26,9 @@ $(document).ready(() => {
     });
     $(".right").on("click", function() { 
         let word = $(".right").attr("id");
+        synth.speak(new SpeechSynthesisUtterance(word));
         console.log(word);
+
         let lyric = "I have a pet " + word;
         $(".lyric").text(lyric);
         state = pushLyric(lyric, songs, state);
