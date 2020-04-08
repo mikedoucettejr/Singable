f250d29aaaeddd087b7139ed9c287af9c5a51c39
Natalie Russell <nlrussell97@gmail.com>
2020-04-08 15:13:25 -0500

    Improved accessibility features and data mgmt

--- engine.js
+++ engine.js
@@ -1,50 +1,42 @@
 $(document).ready(() => {
     let synth = window.speechSynthesis;
 
-    // read aloud the blank lyric
-    $("#speak").on("click", function() { 
-        // have to edit lyric so it reads out 'blank' rather than 'underscore' x5
-        let currentLyric = $(".lyric").text().replace(/_____/, 'blank'); 
-        let utterThis = new SpeechSynthesisUtterance(currentLyric);
-        synth.speak(utterThis);
-    });
-    
-    let songs = [{}]; // this is where each lyric will be stored as an object
-    let state = {songID: 0, lyricCount: 0};
-    // all objects with same songID belong to the same song
+    // space bar reads current lyric
+    document.body.onkeyup = function(e) {
+        if (e.keyCode == 32) { 
+            // have to edit lyric so it reads out 'blank' rather than 'underscore' x5
+            let currentLyric = $(".lyric").text().replace(/_____/, 'blank');
+            let utterThis = new SpeechSynthesisUtterance(currentLyric);
+            synth.speak(utterThis);
+        }
+    }
 
-    $(".left").on("click", function() { 
+    let song = {lyrics: ""}; // this is where each lyric will be stored as an object
+
+    $(".left").on("click", function () {
         let word = $(".left").attr("id");
         synth.speak(new SpeechSynthesisUtterance(word));
         console.log(word);
-
         let lyric = "I have a pet " + word;
         $(".lyric").text(lyric);
-        state = pushLyric(lyric, songs, state);
-        // console.log(songs);
-        // console.log(state);
     });
-    $(".right").on("click", function() { 
+    $(".right").on("click", function () {
         let word = $(".right").attr("id");
         synth.speak(new SpeechSynthesisUtterance(word));
         console.log(word);
-
         let lyric = "I have a pet " + word;
         $(".lyric").text(lyric);
-        state = pushLyric(lyric, songs, state);
-        // console.log(songs);
-        // console.log(state);
+    });
+    $("#next").on("click", function () {
+        let lyric = $(".lyric").text();
+        pushLyric(lyric, song);
+        console.log(song);
     });
 });
 
-let pushLyric = (lyric, songs, state) => {
+let pushLyric = (lyric, song) => {
     // adds a lyric to the songs object! 
     console.log("Pushing lyric");
-    songs[state.lyricCount] = {
-        id: state.songID,
-        lyric: lyric
-    };
-    state.lyricCount++;
-    return state;
+    song.lyrics += lyric + " ";
 };
 
