17f2997c42011745c49cdb19deac9c9af175242e
Natalie Russell <nlrussell97@gmail.com>
2020-04-15 16:38:27 -0400

    Added functionality for all 4 lyrics and movement b/t them

--- engine.js
+++ engine.js
@@ -1,36 +1,33 @@
 $(document).ready(() => {
-    let synth = window.speechSynthesis;
+    let lyricNum = 0;
+    let allLyrics = ["I have a pet ", "It says ", "And when I play the ", "Together we make "];
+    let options = [{ left: "dog", right: "rabbit" }, { left: "meow", right: "ribbit" }, { left: "piano", right: "toothbrush" }, { left: "music", right: "dinner" }];
+    let song = { lyrics: "" }; // this is where each lyric will be stored as an object
+
+    renderNextPage(allLyrics, lyricNum, options); // generates HTML for specific lyric
 
     // space bar reads current lyric
-    document.body.onkeyup = function(e) {
-        if (e.keyCode == 32) { 
+    // **TODO**: space bar also clicks the button you last clicked if it's still selected 
+    document.body.onkeyup = function (e) {
+        if (e.keyCode == 32) {
             // have to edit lyric so it reads out 'blank' rather than 'underscore' x5
             let currentLyric = $(".lyric").text().replace(/_____/, 'blank');
             let utterThis = new SpeechSynthesisUtterance(currentLyric);
-            synth.speak(utterThis);
+            window.speechSynthesis.speak(utterThis);
         }
     }
 
-    let song = {lyrics: ""}; // this is where each lyric will be stored as an object
-
-    $(".left").on("click", function () {
-        let word = $(".left").attr("id");
-        synth.speak(new SpeechSynthesisUtterance(word));
-        console.log(word);
-        let lyric = "I have a pet " + word;
-        $(".lyric").text(lyric);
-    });
-    $(".right").on("click", function () {
-        let word = $(".right").attr("id");
-        synth.speak(new SpeechSynthesisUtterance(word));
-        console.log(word);
-        let lyric = "I have a pet " + word;
-        $(".lyric").text(lyric);
-    });
     $("#next").on("click", function () {
         let lyric = $(".lyric").text();
         pushLyric(lyric, song);
-        console.log(song);
+        lyricNum++;
+        if (lyricNum == allLyrics.length) { // you've completed the game!
+            $('#root').html(`<p class="lyric">Great Job!</p>`);
+            console.log(song);
+        } else { // move to next page of lyrics and options
+            renderNextPage(allLyrics, lyricNum, options);
+            console.log(song);
+        }
     });
 });
 
@@ -40,3 +37,38 @@ let pushLyric = (lyric, song) => {
     song.lyrics += lyric + " ";
 };
 
+let setupOptionClickHandlers = (allLyrics, lyricNum) => {
+    $(".left").on("click", function () {
+        let word = $(".left").attr("id");
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(word));
+        console.log(word);
+        let lyric = allLyrics[lyricNum] + word;
+        $(".lyric").text(lyric);
+    });
+    $(".right").on("click", function () {
+        let word = $(".right").attr("id");
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(word));
+        console.log(word);
+        let lyric = allLyrics[lyricNum] + word;
+        $(".lyric").text(lyric);
+    });
+};
+
+let renderNextPage = (allLyrics, lyricNum, options) => {
+    const $root = $('#root');
+    let html = `<div>
+   <p class="lyric">${allLyrics[lyricNum]}_____</p>
+    </div>
+    <div class="options">
+            <div class="pic">
+                <button class="left" id="${options[lyricNum].left}">
+                <img src="./assets/images/${options[lyricNum].left}.jpg" alt="${options[lyricNum].left}"></button>
+            </div>
+            <div class="pic">
+                <button class="right" id="${options[lyricNum].right}">
+                <img src="./assets/images/${options[lyricNum].right}.jpg" alt="${options[lyricNum].right}"></button>
+            </div>
+    </div>`;
+    $root.html(html);
+    setupOptionClickHandlers(allLyrics, lyricNum);
+};
