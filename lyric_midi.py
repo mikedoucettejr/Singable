879ce7690d0c611cc68d97733323b04a81976dc1
Mike Doucette <mikedoucettejr@gmail.com>
2020-04-18 12:34:17 -0400

    update to include wav and fix requirements

--- lyric_midi.py
+++ lyric_midi.py
@@ -8,6 +8,8 @@ from matplotlib.patches import Polygon
 import os
 from gensim.models import Word2Vec #Genism 3.6
 from sklearn.neighbors import KernelDensity
+import scipy.io.wavfile as wavf
+import pretty_midi
 
 
 #Create dictionary
@@ -27,10 +29,7 @@ syllable_dict = dict(zip(word_list_dict, syl_list_dict))
 
 # takes LYRIC as STR (no punctuation)
 # outputs MIDI file
-def lyric_to_midi(lyrics):
-    syll_model_path = 'assets/data/enc_models/syllEncoding_20190419.bin'
-    word_model_path = 'assets/data/enc_models/wordLevelEncoder_20190419.bin'
-
+def get_syl_lyric_list(lyrics):
     syl_lyr_list = []
     for word in lyrics.split(' '):
         try:
@@ -40,7 +39,36 @@ def lyric_to_midi(lyrics):
         for syl in syl_list:
             syl_lyr_list.append([syl, word])
     
-    lyrics = syl_lyr_list
+    return syl_lyr_list
+    
+def create_midi_pattern_w_sample (discretized_sample, syl_list):
+    new_midi = pretty_midi.PrettyMIDI()
+    voice = pretty_midi.Instrument(1)  # It's here to change the used instruments !
+    tempo = 120
+    ActualTime = 0  # Time since the beginning of the song, in seconds
+    for i in range(0,len(discretized_sample)):
+        length = discretized_sample[i][1] * 60 / tempo  # Conversion Duration to Time
+        if i < len(discretized_sample) - 1:
+            gap = discretized_sample[i + 1][2] * 60 / tempo
+        else:
+            gap = 0  # The Last element doesn't have a gap
+        note = pretty_midi.Note(velocity=100, pitch=int(discretized_sample[i][0]), start=ActualTime,
+                                end=ActualTime + length)
+        voice.notes.append(note)
+        ActualTime += length + gap  # Update of the time
+
+    new_midi.instruments.append(voice)
+
+    return new_midi
+
+def lyric_to_wav(lyrics):
+    syll_model_path = 'assets/data/enc_models/syllEncoding_20190419.bin'
+    word_model_path = 'assets/data/enc_models/wordLevelEncoder_20190419.bin'
+    
+    lyrics = get_syl_lyric_list(lyrics)
+    syl_list = []
+    for s_list in lyrics:
+        syl_list.append(s_list[0])
 
     syllModel = Word2Vec.load(syll_model_path)
     wordModel = Word2Vec.load(word_model_path)
@@ -53,6 +81,7 @@ def lyric_to_midi(lyrics):
             syll2Vec = syllModel.wv[lyrics[i][0]]
             word2Vec = wordModel.wv[lyrics[i][1]]
             cond.append(np.concatenate((syll2Vec,word2Vec)))
+            #print(lyrics[i][1])
         else: # just sylables
             cond.append(np.concatenate((syll2Vec,word2Vec)))
 
@@ -63,8 +92,7 @@ def lyric_to_midi(lyrics):
 
     model_path = 'assets/data/saved_gan_models/1August/epochs_models/model_epoch395'
 
-    x_list = []
-    y_list = []
+    lyric_len = len(lyrics)
 
     with tf.Session(graph=tf.Graph()) as sess:
         tf.saved_model.loader.load(sess, [], model_path)
@@ -83,9 +111,15 @@ def lyric_to_midi(lyrics):
         generated_features = sess.run(output_midi, feed_dict)
         sample = [x[0, :] for x in generated_features]
         sample = lyric_midi_utils.tune_song(lyric_midi_utils.discretize(sample))
-        midi_pattern = lyric_midi_utils.create_midi_pattern_from_discretized_data(sample[0:length_song])
-        destination = "test.mid"
-        midi_pattern.write(destination)
+        midi_pattern = create_midi_pattern_w_sample(sample[0:length_song], syl_list)
+        #destination = "test.mid"
+        #midi_pattern.write(destination)
+
+        audio_data = midi_pattern.synthesize() # synthesizes into numpy array (with wav encoding)
+        fs = 44100
+        out_f = 'out.wav'
+
+        wavf.write(out_f, fs, audio_data) # writes audio data to above location with numpy array & sample rate
+        print('wav written to "out.wav" & audio numpy returned')
     
-    
-    return destination
+    return audio_data # returning numpy array of audio data (wav is written to 'out.wav')
