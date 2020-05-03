8a30b8f3ab31470096e807a11e5b3cbca7601c58
Mike Doucette <mikedoucettejr@gmail.com>
2020-05-03 12:52:40 -0400

    removed extra file

--- test-lyric.py
+++
@@ -1,6 +0,0 @@
-import lyric_midi
-
-audio_array = lyric_midi.lyric_to_wav('yellow floors on the east side of here')
-print(audio_array)
-
-print(lyric_midi.get_syl_lyric_list('yellow floors on the east side of here'))
