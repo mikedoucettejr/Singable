3a7d7e37cfa5c923bc4433062ad7bac40487767d
Mike Doucette <mikedoucettejr@gmail.com>
2020-05-03 12:50:57 -0400

    overlay fully functional

--- lyric_midi.py
+++ lyric_midi.py
@@ -8,9 +8,10 @@ import lyric_midi_utils
 import os
 from gensim.models import Word2Vec #Genism 3.6
 from sklearn.neighbors import KernelDensity
-import scipy.io.wavfile as wavf
+import soundfile as sf
 import pretty_midi
-
+from gtts import gTTS
+from pydub import AudioSegment
 
 #Create dictionary
 #https://www.gutenberg.org/ebooks/3204
@@ -43,10 +44,11 @@ def get_syl_lyric_list(lyrics):
     
     return syl_lyr_list
     
-def create_midi_pattern_w_sample (discretized_sample, syl_list):
+def create_midi_pattern_w_sample (discretized_sample, lyrics):
     new_midi = pretty_midi.PrettyMIDI()
-    voice = pretty_midi.Instrument(1)  # It's here to change the used instruments !
-    tempo = 120
+    bass = pretty_midi.Instrument(33)  # It's here to change the used instruments !
+    treble = pretty_midi.Instrument(21)  # It's here to change the used instruments !    
+    tempo = 182
     ActualTime = 0  # Time since the beginning of the song, in seconds
     for i in range(0,len(discretized_sample)):
         length = discretized_sample[i][1] * 60 / tempo  # Conversion Duration to Time
@@ -54,12 +56,22 @@ def create_midi_pattern_w_sample (discretized_sample, syl_list):
             gap = discretized_sample[i + 1][2] * 60 / tempo
         else:
             gap = 0  # The Last element doesn't have a gap
-        note = pretty_midi.Note(velocity=100, pitch=int(discretized_sample[i][0]), start=ActualTime,
+        bass_note = pretty_midi.Note(velocity=100, pitch=int(discretized_sample[i][0]-3), start=ActualTime,
                                 end=ActualTime + length)
-        voice.notes.append(note)
+        bass.notes.append(bass_note)
+        #treble_note = pretty_midi.Note(velocity=100, pitch=int(discretized_sample[i][0]+12), start=ActualTime,
+        #                        end=ActualTime + length)
+        #treble.notes.append(treble_note)
+
+        if i < len(lyrics): 
+            lyric_obj = pretty_midi.Lyric(lyrics[i], ActualTime)
+            new_midi.lyrics.append(lyric_obj)
+            #print(lyric_obj)
+
         ActualTime += length + gap  # Update of the time
 
-    new_midi.instruments.append(voice)
+    new_midi.instruments.append(bass)
+    #new_midi.instruments.append(treble)
 
     return new_midi
 
@@ -67,6 +79,8 @@ def lyric_to_wav(lyrics):
     syll_model_path = 'assets/data/enc_models/syllEncoding_20190419.bin'
     word_model_path = 'assets/data/enc_models/wordLevelEncoder_20190419.bin'
     
+    orig_lyric = lyrics
+    lyric_str = lyrics.split(' ')
     lyrics = get_syl_lyric_list(lyrics)
     syl_list = []
     for s_list in lyrics:
@@ -80,10 +94,13 @@ def lyric_to_wav(lyrics):
 
     for i in range(20):
         if i < length_song: # both
-            syll2Vec = syllModel.wv[lyrics[i][0]]
-            word2Vec = wordModel.wv[lyrics[i][1]]
+            try:
+                syll2Vec = syllModel.wv[lyrics[i][0]]
+                word2Vec = wordModel.wv[lyrics[i][1]]
+            except KeyError:
+                syll2Vec = syllModel.wv[lyrics[i-1][0]] #Potential index error
+                word2Vec = wordModel.wv[lyrics[i-1][0]]
             cond.append(np.concatenate((syll2Vec,word2Vec)))
-            #print(lyrics[i][1])
         else: # just sylables
             cond.append(np.concatenate((syll2Vec,word2Vec)))
 
@@ -95,6 +112,7 @@ def lyric_to_wav(lyrics):
     model_path = 'assets/data/saved_gan_models/1August/epochs_models/model_epoch395'
 
     lyric_len = len(lyrics)
+    print(lyric_str)
 
     with tf.Session(graph=tf.Graph()) as sess:
         tf.saved_model.loader.load(sess, [], model_path)
@@ -113,19 +131,37 @@ def lyric_to_wav(lyrics):
         generated_features = sess.run(output_midi, feed_dict)
         sample = [x[0, :] for x in generated_features]
         sample = lyric_midi_utils.tune_song(lyric_midi_utils.discretize(sample))
-        midi_pattern = create_midi_pattern_w_sample(sample[0:length_song], syl_list)
+        midi_pattern = create_midi_pattern_w_sample(sample[0:length_song], lyric_str)
+
+        lyric_midi_utils.create_midi_pattern_from_discretized_data
         #destination = "test.mid"
         #midi_pattern.write(destination)
 
-        audio_data = midi_pattern.synthesize() # synthesizes into numpy array (with wav encoding)
-        fs = 44100
-        out_f = 'out.wav'
-
-        wavf.write(out_f, fs, audio_data) # writes audio data to above location with numpy array & sample rate
-        print('wav written to "out.wav" & audio numpy returned')
+        audio_data = wav_from_midi(midi_pattern)
     
-    return audio_data # returning numpy array of audio data (wav is written to 'out.wav')
+    overlay_song_from_lyric(orig_lyric)
+    return 'overlayed.wav' # returning numpy array of audio data (wav is written to 'out.wav')
 
 
-#audio_array = lyric_to_wav('One step ahead is a step too far away from you')
-#print('audio array complete')
+def wav_from_midi(midi):
+    audio_data = midi.synthesize() # synthesizes into numpy array (with wav encoding)
+    sf.write('backing.wav', audio_data, 44100)
+    print('wav written to "backing.wav" & audio numpy returned')
+
+    return audio_data
+
+def overlay_song_from_lyric(lyric):
+    
+    tts = gTTS(lyric, slow=True)
+    tts.save('text.mp3')
+    
+    backing = AudioSegment.from_file("backing.wav")
+    text = AudioSegment.from_file("text.mp3")
+
+    quieter_backing = backing.apply_gain(-1.5)
+    quieter_text = text.apply_gain(-3.0)
+
+    played_togther = quieter_backing.overlay(quieter_text)
+    
+    played_togther.export('overlayed.wav')
+    print("final wav file printed to 'overlayed.wav'")
--- test-lyric.py
+++ test-lyric.py
@@ -1,4 +1,6 @@
 import lyric_midi
 
-audio_array = lyric_midi.lyric_to_wav('One step ahead is a step too far away from you')
+audio_array = lyric_midi.lyric_to_wav('yellow floors on the east side of here')
 print(audio_array)
+
+print(lyric_midi.get_syl_lyric_list('yellow floors on the east side of here'))
